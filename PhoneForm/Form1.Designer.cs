9c9158ac5bec4a2925a70022004c5c7a7363b802
martafonde <marta4510@hotmail.com>
2020-12-13 19:52:26 +0100

    v1

---
+++ PhoneForm/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace PhoneForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grabarNúmeroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(33, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(138, 75);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Tag = "-1";
+            this.btnReset.Text = "&Borrar";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnReset_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(250, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.grabarNúmeroToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "&Archivo";
+            // 
+            // grabarNúmeroToolStripMenuItem
+            // 
+            this.grabarNúmeroToolStripMenuItem.Name = "grabarNúmeroToolStripMenuItem";
+            this.grabarNúmeroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.grabarNúmeroToolStripMenuItem.Text = "&Grabar número";
+            this.grabarNúmeroToolStripMenuItem.Click += new System.EventHandler(this.grabarNumero);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.resetToolStripMenuItem.Text = "&Borrar";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "&Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.acercaDeToolStripMenuItem.Text = "A&cerca de ...";
+            this.acercaDeToolStripMenuItem.Click += new System.EventHandler(this.acercaDeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 375);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PhoneForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grabarNúmeroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+    }
+}
+
---
+++ PhoneForm/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneForm
+{
+    public partial class Form1 : Form
+    {
+        int[] pin = { 0000, 0147, 0258, 0369 };
+        Form2 f2;
+        Button b;
+        Color tecla = Color.LightGray;
+        Color selecTecla = Color.Crimson;
+        Color focoTecla = Color.LightSlateGray;
+        SaveFileDialog sv;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            
+            int intentos = 3;
+            f2 = new Form2();
+            getText();
+            bool error = true;
+
+            while (error)
+            {
+                if (intentos > 0)
+                {
+                    DialogResult res = f2.ShowDialog();
+                    int num;
+
+                    switch (res)
+                    {
+                        case DialogResult.OK:
+                            if(f2.textBox1.Text.Length == 4)
+                            {
+                                if (int.TryParse(f2.textBox1.Text, out num))
+                                {
+                                    if (!pin.Contains(num))
+                                    {
+                                        f2.lblPin.Text = Strings.pinNoValido + Strings.intentos + (--intentos);
+                                    }
+                                    else
+                                    {
+                                        error = false;
+                                    }
+                                }
+                                else
+                                {
+                                    f2.lblPin.Text = Strings.numPin + Strings.intentos + (--intentos);
+                                }
+                            }
+                            else
+                            {
+                                f2.lblPin.Text = Strings.pinNoValido + Strings.longPin + Strings.intentos + (--intentos);
+                            }
+
+                            break;
+
+                        case DialogResult.Cancel:
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+            if (!error)
+            {
+                int x = 50;
+                int y = 150;
+                for (int i = 1; i <= 12; i++)
+                {
+                    b = new Button();                    
+                    b.Location = new System.Drawing.Point(x, y);
+                    b.Size = new System.Drawing.Size(50, 50);
+                    b.BackColor = tecla;
+                    b.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn_Click);
+                    b.MouseEnter += new System.EventHandler(this.btn_Enter);
+                    b.MouseLeave += new System.EventHandler(this.btn_Leave);
+
+                    if (i == 10)
+                    {
+                        b.Tag = '*';
+                        b.Text += b.Tag;
+                    }
+                    else if (i == 11)
+                    {
+                        b.Tag = 0;
+                        b.Text += b.Tag.ToString();
+                    }
+                    else if(i == 12)
+                    {
+                        b.Tag = '#';
+                        b.Text += b.Tag;
+                    }                    
+                    else
+                    {
+                        b.Tag = i;
+                        b.Text = b.Tag.ToString();
+                    }
+
+                    this.Controls.Add(b);
+
+                    if (i % 3 == 0)
+                    {
+                        y += 50;
+                        x = 50;
+                    }
+                    else
+                    {
+                        x += 50;
+                    }
+                }
+            }              
+        }
+
+        private void getText()
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+            this.Text = Strings.tituloForm;
+            btnReset.Text = Strings.borrar;
+            archivoToolStripMenuItem.Text = Strings.archivo;
+            resetToolStripMenuItem.Text = Strings.borrar;
+            salirToolStripMenuItem.Text = Strings.salir;
+            acercaDeToolStripMenuItem.Text = Strings.acercaDe;
+            grabarNúmeroToolStripMenuItem.Text = Strings.guardar;
+            f2.Text = Strings.introPIN;
+            f2.button1.Text = Strings.aceptar;
+        }
+
+        private void btn_Click(Object sender, MouseEventArgs e)
+        {
+            Button b = (Button)sender;
+            b.BackColor = selecTecla;
+            this.textBox1.Text += b.Tag;
+        }
+
+        private void btn_Enter(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (b.BackColor != selecTecla)
+            {
+                b.BackColor = focoTecla;
+            }
+        }
+
+        private void btn_Leave(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if(b.BackColor != selecTecla)
+            {
+                b.BackColor = tecla;
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is Button)
+                {
+                    c.BackColor = tecla;
+                }
+                if (c is TextBox)
+                {
+                    c.Text = "";
+                }
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == '*' || e.KeyChar == '#')
+            {
+                foreach (Control c in this.Controls)
+                {
+                    if (c is Button)
+                    {
+                        if(c.Tag.ToString() == e.KeyChar.ToString())
+                        {
+                            textBox1.Text += c.Tag.ToString();
+                            c.BackColor = selecTecla;
+                        }
+                    }                    
+                }
+            }
+        }
+
+        private void grabarNumero(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length > 0)
+            {
+                sv = new SaveFileDialog();
+                this.sv.Title = Strings.guardar;
+                this.sv.InitialDirectory = Environment.CurrentDirectory;
+                this.sv.Filter = ".txt |*.txt|"+Strings.todosArchivos+"|*.*";
+                this.sv.ValidateNames = true;
+                DialogResult res = sv.ShowDialog();
+
+                switch (res)
+                {
+                    case DialogResult.OK:
+                        StreamWriter sw;
+                        using (sw = new StreamWriter(this.sv.FileName, true))
+                        {
+                            sw.WriteLine(textBox1.Text);
+                        }
+                        break;
+                }
+            }
+            else
+            {
+               MessageBox.Show(Strings.introduceNum, Strings.errorGuardar, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }            
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show(Strings.confSalir, Strings.conf, MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+                 == DialogResult.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Strings.infoApp, Strings.info, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
